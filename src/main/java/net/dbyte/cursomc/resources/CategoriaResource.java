c28d227d982434a13058a29fdac12e077ac80400
Eduardo Busato Damacena <eduardobusato51@gmail.com>
2018-11-15 19:21:45 -0200

    Testando a primeira classe de domínio - Categoria

--- src/main/java/net/dbyte/cursomc/resources/CategoriaResource.java
+++ src/main/java/net/dbyte/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,30 @@
 package net.dbyte.cursomc.resources;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import net.dbyte.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() 
+	public List<Categoria> listar() 
 	{
-		return "REST funcionando";
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
