9b62a0f9d901070759e787c6b32c459d24893217
Eduardo Busato Damacena <eduardobusato51@gmail.com>
2018-11-17 18:33:31 -0200

    Endpoint /cliente/{id} disponível

---
+++ src/main/java/net/dbyte/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package net.dbyte.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.dbyte.cursomc.domain.Cliente;
+import net.dbyte.cursomc.repositories.ClienteRepository;
+import net.dbyte.cursomc.resources.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id ) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrato! Id: " +
+		id + " , Tipo: " + Cliente.class.getName()));
+	}
+}
