0d857647678956662d12141804fc23aeb3d7f372
Eduardo Busato Damacena <eduardobusato51@gmail.com>
2018-11-20 22:47:34 -0200

    Endpoint /pedidos/{id} disponível

---
+++ src/main/java/net/dbyte/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package net.dbyte.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.dbyte.cursomc.domain.Pedido;
+import net.dbyte.cursomc.repositories.PedidoRepository;
+import net.dbyte.cursomc.resources.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " +
+		id + ", Tipo: " + Pedido.class)); 
+	}
+}
