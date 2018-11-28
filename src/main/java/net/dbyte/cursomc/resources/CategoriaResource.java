11048b5bd01727e390e5ff5527947a48ea319c4f
Eduardo Busato Damacena <eduardobusato51@gmail.com>
2018-11-28 15:54:46 -0200

    Inserindo novo Cliente com POST

--- src/main/java/net/dbyte/cursomc/resources/CategoriaResource.java
+++ src/main/java/net/dbyte/cursomc/resources/CategoriaResource.java
@@ -38,7 +38,7 @@ public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDTO) {
 		Categoria obj = service.fromDTO(objDTO);
-		service.insert(obj);
+		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
 				path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
--- src/main/java/net/dbyte/cursomc/services/ClienteService.java
+++ src/main/java/net/dbyte/cursomc/services/ClienteService.java
@@ -3,6 +3,8 @@ package net.dbyte.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import net.dbyte.cursomc.domain.Cidade;
 import net.dbyte.cursomc.domain.Cliente;
+import net.dbyte.cursomc.domain.Endereco;
+import net.dbyte.cursomc.domain.enums.TipoCliente;
 import net.dbyte.cursomc.dto.ClienteDTO;
+import net.dbyte.cursomc.dto.ClienteNewDTO;
 import net.dbyte.cursomc.repositories.ClienteRepository;
+import net.dbyte.cursomc.repositories.EnderecoRepository;
 import net.dbyte.cursomc.resources.exception.DataIntegrityException;
 import net.dbyte.cursomc.resources.exception.ObjectNotFoundException;
 
@@ -22,10 +29,21 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	public Cliente find(Integer id ) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrato! Id: " +
 		id + " , Tipo: " + Cliente.class.getName()));
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -57,6 +75,20 @@ public class ClienteService {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
 	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if (objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if (objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
+	}
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
