8b5e7bacaca5c15f17e1dc94f6a91c1a9c9f04f0
Eduardo Busato Damacena <eduardobusato51@gmail.com>
2018-12-17 22:37:51 -0200

    Validação customizada: email não repetido na atualização de Cliente

---
+++ src/main/java/net/dbyte/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,51 @@
+package net.dbyte.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import net.dbyte.cursomc.domain.Cliente;
+import net.dbyte.cursomc.dto.ClienteDTO;
+import net.dbyte.cursomc.repositories.ClienteRepository;
+import net.dbyte.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDTO, ConstraintValidatorContext context) {
+		Map <String, String> map = (Map <String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = repo.findByEmail(objDTO.getEmail());
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "E-Mail já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
